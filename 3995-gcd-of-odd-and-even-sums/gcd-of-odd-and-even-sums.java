b04d3ade1050dff055a8c57cd17745c8ea069b23
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-07-15 09:04:18 +0530

    Time: 1 ms (83.11%) | Memory: 42.5 MB (74.78%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int i=1;
+        int odd=0;
+        int even =0;
+        while(i<=n){
+            odd+=(2*i-1);
+            even+=(2*i);
+            i++;
+        }
+        return calgcd(odd,even);
+    }
+    private int calgcd(int a , int b){
+        while(b!=0){
+            int temp =b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
+}
