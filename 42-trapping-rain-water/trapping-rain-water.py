8b682504d949aa26ebf6b51e8c3dec109282995b
Vinish V <138119009+vinish-v@users.noreply.github.com>
2026-07-21 08:35:42 +0530

    Time: 7 ms (74.27%) | Memory: 21 MB (48.21%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left =0
+        right =len(height)-1
+        leftmax =0
+        rightmax =0
+        res =0
+        while left <= right:
+            if height[left] <= height[right]:
+                if height[left] >= leftmax:
+                    leftmax = height[left]
+                else:
+                    res+= leftmax - height[left]
+                left+=1
+            else:
+                if height[right]>=rightmax:
+                    rightmax = height[right]
+                else:
+                    res+=rightmax -height[right]
+                right-=1
+        return res
